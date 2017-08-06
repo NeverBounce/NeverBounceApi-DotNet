75360bdf1c5d1a2d3a1e16464cb04ab50f2004a8
Mike Mollick <mollick2@gmail.com>
2017-08-06 15:19:13 -0400

    Adding proper exceptions

---
+++ NeverBounceSDK/NeverBounceSDK/Exceptions/BadReferrerException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace NeverBounce.Exceptions
+{
+    [Serializable()]
+    public class GeneralException : Exception
+    {
+		public GeneralException() : base() { }
+		public GeneralException(string message) : base(message) { }
+		public GeneralException(string message, Exception inner) : base(message, inner) { }
+
+		// A constructor is needed for serialization when an
+		// exception propagates from a remoting server to the client. 
+		protected GeneralException(System.Runtime.Serialization.SerializationInfo info,
+			System.Runtime.Serialization.StreamingContext context)
+		{ }
+    }
+}
