960783e1151fefde83c4e94cf94edfb7251cec2a
Keith Henry <keithhenry@gmail.com>
2023-07-03 12:38:54 +0100

    fix: snake case parsing

--- NeverBounceSDK/Utilities/SnakeCase.cs
+++ NeverBounceSDK/Utilities/SnakeCase.cs
@@ -2,28 +2,36 @@
 using System.Text;
 using System.Text.Json;
 
-/// <summary>JSON naming policy to convert .NET MyClass into my_class used by OpenAI</summary>
+/// <summary>JSON naming policy to convert .NET MyClass into my_class</summary>
 class SnakeCase : JsonNamingPolicy
 {
-    public override string ConvertName(string name)
-    {
+    public override string ConvertName(string name) => Convert(name);
 
+    public static string Convert(string name) {
         if (string.IsNullOrEmpty(name)) return name;
 
         if (name.Length < 3)
-            return name.ToLowerInvariant(); // don't turn names like "ID" into "i_d", but "TopP" should become "top_p"
+            return name.ToLowerInvariant(); // short-cut, 2 chars will always be all lowercase
 
         var sb = new StringBuilder();
-        sb.Append(char.ToLowerInvariant(name[0]));
+        char first = name[0];
+        bool prevUpper = char.IsUpper(name[0]);
+        sb.Append(char.ToLowerInvariant(first));
         for (int i = 1; i < name.Length; i++)
         {
             if (char.IsUpper(name[i]))
             {
-                sb.Append('_');
+                if (!prevUpper)
+                    sb.Append('_'); // avoid chain of _ (e.g. AbcDEF goes to abc_def, not abc_d_e_f)
+
+                prevUpper = true;
                 sb.Append(char.ToLowerInvariant(name[i]));
             }
             else
+            {
+                prevUpper = false;
                 sb.Append(name[i]);
+            }
         }
 
         return sb.ToString();
